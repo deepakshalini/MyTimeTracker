e801d4a30b62a35aa546b24fdb5ff24d87f6a3cd
deepak <sheenusoni09@gmail.com>
2026-06-06 00:09:40 +0530

    pdf generator modification

--- test_pdf_generator.py
+++ test_pdf_generator.py
@@ -381,7 +381,7 @@ def generate_pdf(filename, task, subtasks, total_hours, total_amount, report_id=
         text_stack.setStyle(TableStyle([("VALIGN", (0, 0), (-1, -1), "TOP"), ("LEFTPADDING", (0, 0), (-1, -1), 0),
                                         ("TOPPADDING", (0, 0), (-1, -1), 0)]))
 
-        block = Table([[icon(icon_name, 16), text_stack]], colWidths=[24, 146])
+        block = Table([[icon(icon_name, 20), text_stack]], colWidths=[30, 140])
         block.setStyle(TableStyle([("VALIGN", (0, 0), (-1, -1), "MIDDLE"), ("LEFTPADDING", (0, 0), (-1, -1), 0)]))
         return block
 
@@ -606,6 +606,7 @@ def generate_pdf(filename, task, subtasks, total_hours, total_amount, report_id=
 
     table.setStyle(TableStyle(tbl_style))
     elements.append(table)
+    elements.append(Spacer(1, 20))
 
     # ======================================================================
     # KEY DELIVERABLES SECTION
